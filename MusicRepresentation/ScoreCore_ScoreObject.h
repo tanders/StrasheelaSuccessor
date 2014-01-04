eef1653c90d26249994d8944e1892e788a1451c2
Torsten Anders <torsten.anders@beds.ac.uk>
2014-01-04 12:28:58 +0000

    Added incomplete type/class defs to allow compilation again.

--- MusicRepresentation/ScoreCore_ScoreObject.h
+++ MusicRepresentation/ScoreCore_ScoreObject.h
@@ -9,23 +9,29 @@
 #ifndef __MusicRepresentation__ScoreScore_ScoreObject__
 #define __MusicRepresentation__ScoreScore_ScoreObject__
 
+#include <map>
+#include <vector>
+#include <boost/variant.hpp>
+
+class ScoreObject;
+typedef std::map<std::string, boost::variant<int,std::string,ScoreObject,std::vector<ScoreObject>>> args;
+
 #include "ScoreCore_TypeArgs.h"
 
 /** [abstract class] The most general data type for score data is a ScoreObject. info can store arbitrary user information (internally a vector of strings).
  */
 class ScoreObject {
     //    int id;  // Perhaps I need id later again...
-    // !! TODO: does class destructor explicitly need to delete any internal vector?
     std::vector<std::string> info;
     
 public:
     ScoreObject(args as);
+    
     std::vector<std::string> getInfo(void);
     void addInfo(std::string myInfo);
     bool hasThisInfo(std::string myInfo);
     
-    // !! TODO -- many member functions still missing -- see Strasheela source
-    
+    #warning TODO -- many member functions still missing -- see Strasheela source
 };
 
 

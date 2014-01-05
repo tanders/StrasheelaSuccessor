221b169ad050bd55d182302648e25b4df15360d7
Torsten Anders <torsten.anders@beds.ac.uk>
2014-01-05 20:38:07 +0000

    Link added to CATCH to easily find unit testing doc

--- UnitTesting/main.cpp
+++ UnitTesting/main.cpp
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 torsten. All rights reserved.
 //
 
+// Unit Testing with CATCH, see
+// https://github.com/philsquared/Catch
 
 // This tells Catch to provide a main() - only do this in one cpp file
 // Command line options of resulting execuable:

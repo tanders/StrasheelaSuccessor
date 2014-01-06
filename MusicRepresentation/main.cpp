8ac9faf8b64fe5902481f86e5d9066f887ffe18c
Torsten Anders <torsten.anders@beds.ac.uk>
2014-01-06 01:34:59 +0000

    Removed from comment code snippets that have been included in unit tests now

--- MusicRepresentation/main.cpp
+++ MusicRepresentation/main.cpp
@@ -21,24 +21,14 @@ int main(int argc, const char * argv[])
 {
     
     
-//    ScoreObject(Args {{"info", "bla"}});
-//    ScoreObject scoreObject = {Args {{"info", "bla"}}}; // working
-//    ScoreObject scoreObject = {Args {{"info", 42}}}; // throws as expected
-//    ScoreObject scoreObject = {Args {{"info", "bla"}, {"buggy_arg", "value"}}}; // successfully throws exception
-//    scoreObject.addInfo("test 2");
-    
-    
 //    Parameter p = {Args {{"info", "bla"}, {"value", 41}}};
 //    TimePoint p = {Args {{"value", 0}, {"unit",  "beats"}}};
 
-////    cout << "hasThisInfo? " << scoreObject.hasThisInfo("bla") << "\n";
-//    cout << "hasThisInfo? " << p.hasThisInfo("bla") << "\n";
-//    cout << "value:  " << p.getValue() << "\n";
-//    cout << "unit :  " << p.getUnit() << "\n";
-    
+    //    cout << "value:  " << p.getValue() << "\n";
+    //    cout << "unit :  " << p.getUnit() << "\n";
+
 //    Element e = {Args {{"info", "bla"}, {"endTime", 10}, {"duration", 10}}};
 //    
-//    //    cout << "hasThisInfo? " << scoreObject.hasThisInfo("bla") << "\n";
 //    // TMP comment
 //    cout << "hasThisInfo? " << e.hasThisInfo("bla") << "\n";
 //    cout << "startTime:  " << e.getStartTime() << "\n";
